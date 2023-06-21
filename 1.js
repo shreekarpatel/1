8587079b27c7ab4e813f05f864ed18b225668358
shreekar patel <shreekarpatel8055@gmail.com>
2023-06-21 12:04:23 +0530

    third

---
+++ 1.js
@@ -0,0 +1,10 @@
+function download(filename, text) {
+  var pom = document.createElement('a');
+  pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  pom.setAttribute('download', filename);
+  pom.style.display = 'none';
+  document.body.appendChild(pom);
+  pom.click();
+  document.body.removeChild(pom);
+}
+
